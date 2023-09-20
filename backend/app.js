c53ebfdd7aa7aa90d8c55cf83e4fe866238d6e82
maryll.driz <maryll.driz@gmail.com>
2023-09-21 01:15:33 +0800

    authUser.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const cookieParser = require('cookie-parser')
 
 const errorMiddleware = require('./middlewares/errors');
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,15 +2,18 @@ const express = require('express');
 const app = express.Router();
 
 const productController = require('../controllers/product.controllers');
+const auth = require('../middlewares/authUser');
+const { isAuthenticatedUser } = auth
+
 
 app.route('/products').get(productController.getProducts);
 app.route('/product/:id').get(productController.getSingleProduct)
 
 
-app.route('/admin/product/:id').put(productController.updateSingleProduct)
+app.route('/admin/product/:id').put(isAuthenticatedUser, productController.updateSingleProduct)
 
 
-app.route('/admin/product/:id').delete(productController.deleteSingleProduct)
+app.route('/admin/product/:id').delete(isAuthenticatedUser, productController.deleteSingleProduct)
 
 
 app.route('/admin/product/new').post(productController.newProduct)
