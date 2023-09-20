f4388e37ac9691b2d86e4040df8c776318d870f9
maryll.driz <maryll.driz@gmail.com>
2023-09-20 20:38:55 +0800

    adding products API

---
+++ backend/models/product.js
@@ -0,0 +1,107 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    productName: {
+        type: String,
+        required: [true, "Please enter product name"],
+    },
+    description: {
+        type: String,
+        required: [true, "Please enter product description"]
+    },
+
+    ratings: {
+        type: Number,
+        default: 0
+    },
+
+    images: [
+        {
+            public_id: {
+                type: String,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+
+    category: {
+        type: String,
+        required: [true, "Please select category for this product"],
+        enum: {
+            values: [
+                'Electronics',
+                'Cameras',
+                'Laptops',
+                'Accessories',
+                'Headphones',
+                'Food',
+                'Books',
+                'Clothes',
+                'Beauty/Health',
+                'Sports',
+                'Home Furnitures'
+            ],
+            message: "Please select correct category for product"
+        }
+    },
+
+    stock: {
+        type: Number,
+        required: [true, 'Please enter product stock']
+    },
+
+    price: {
+        type: Number,
+        required: [true, "Please add a price"]
+    },
+
+    isActive : {
+        type : Boolean,
+        default : true
+    },
+
+    numOfReviews:{
+        type: Number,
+        default: 0
+    },
+
+    reviews: [
+        {
+            name:{
+                type: String,
+                required: true
+            },
+            rating: {
+                type: Number,
+                required: true
+            },
+            comment: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+
+    userOrders : [
+        {
+            userId : {
+                type : Object
+            },
+            orderId : {
+                type : String
+            }
+        }
+    ],
+
+    createdOn : {
+        type : Date,
+        default : Date.now()
+    }
+
+})
+
+module.exports = mongoose.model('Product', productSchema)
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express.Router();
 
-const { getProducts } = require('../controllers/product.controllers');
+const { getProducts, newProduct } = require('../controllers/product.controllers');
 
 app.route('/products').get(getProducts);
+app.route('/product/new').post(newProduct)
 
 module.exports = app;
