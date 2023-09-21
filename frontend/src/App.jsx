b4829e6dbd9bc45fcacbeed9abe9ebf0340cb58a
maryll.driz <maryll.driz@gmail.com>
2023-09-21 20:38:32 +0800

    adding home.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,7 @@
+import  {BrowserRouter as Router } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap';
+
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import Home from './components/Home'
@@ -5,14 +9,22 @@ import Home from './components/Home'
 function App() {
 
   return (
-    <>
+    <Router>
       <div className="App">
        <Header />
-       <Home />
+
+        <Container>
+          <Routes>
+            <Route path= "/" element={<Home />} />
+
+          </Routes>
+        </Container>
+
        <Footer />
       </div>
+    </Router>
       
-    </>
+    
   )
 }
 
