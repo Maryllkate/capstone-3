594261c6d45955aba9fc6846be157fe7f8d4a4a6
maryll.driz <maryll.driz@gmail.com>
2023-09-21 15:20:35 +0800

    route product

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -8,14 +8,8 @@ const { authUser, authRoles } = auth
 
 app.route('/products').get(productController.getProducts);
 app.route('/product/:id').get(productController.getSingleProduct)
-
-
 app.route('/admin/product/:id').put(authUser, authRoles('admin'), productController.updateSingleProduct)
-
-
 app.route('/admin/product/:id').delete(authUser, authRoles('admin'), productController.deleteSingleProduct)
-
-
 app.route('/admin/product/new').post(authUser, authRoles('admin'), productController.newProduct)
 
 module.exports = app;
