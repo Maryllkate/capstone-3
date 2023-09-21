6a62b12294afa2232d6817e4cc83ec87836883fa
maryll.driz <maryll.driz@gmail.com>
2023-09-21 20:20:49 +0800

    header, footer & homepage

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,162 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div classNameName="container container-fluid">
+        <h1 id="products_heading">Latest Products</h1>
+
+        <section id="products" className="container mt-5">
+        <div className="row">
+            <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+                <div className="card p-3 rounded">
+                    <img
+                    className="card-img-top mx-auto"
+                    src="../images/astro.png"
+                    />
+
+                    <div className="card-body d-flex flex-column">
+                        <h5 className="card-title">
+                            <a href="../images/astro.png">128GB Solid Storage Memory card - SanDisk Ultra</a>
+                        </h5>
+                    <div className="ratings mt-auto">
+                        <div className="rating-outer">
+                            <div className="rating-inner"></div>
+                        </div>
+                        <span id="no_of_reviews">(5 Reviews)</span>
+                    </div>
+                        <p className="card-text">PHP 45.67</p>
+                        <a href="#" id="view_btn" className="btn btn-block">View Details</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+                <div className="card p-3 rounded">
+                    <img
+                    className="card-img-top mx-auto"
+                    src="../images/astro.png"
+                    />
+
+                    <div className="card-body d-flex flex-column">
+                        <h5 className="card-title">
+                            <a href="../images/astro.png">Wyze Cam 1080p HD Indoor Wireless Smart Home Camera Wyze Cam 1080p HD Indoor Wireless Smart Home Camera</a>
+                        </h5>
+                    <div className="ratings mt-auto">
+                        <div className="rating-outer">
+                            <div className="rating-inner"></div>
+                        </div>
+                        <span id="no_of_reviews">(5 Reviews)</span>
+                    </div>
+                        <p className="card-text">PHP 45.67</p>
+                        <a href="#" id="view_btn" className="btn btn-block">View Details</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+                <div className="card p-3 rounded">
+                    <img
+                    className="card-img-top mx-auto"
+                    src="../images/astro.png"
+                    />
+
+                    <div className="card-body d-flex flex-column">
+                        <h5 className="card-title">
+                            <a href="../images/astro.png">Fujifilm Instax Mini Instant Film Twin Pack (White)</a>
+                        </h5>
+                    <div className="ratings mt-auto">
+                        <div className="rating-outer">
+                            <div className="rating-inner"></div>
+                        </div>
+                        <span id="no_of_reviews">(5 Reviews)</span>
+                    </div>
+                        <p className="card-text">PHP 45.67</p>
+                        <a href="#" id="view_btn" className="btn btn-block">View Details</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+                <div className="card p-3 rounded">
+                    <img
+                    className="card-img-top mx-auto"
+                    src="../images/astro.png"
+                    />
+
+                    <div className="card-body d-flex flex-column">
+                        <h5 className="card-title">
+                            <a href="../images/astro.png">AmazonBasics High-Speed HDMI Cable</a>
+                        </h5>
+                    <div className="ratings mt-auto">
+                        <div className="rating-outer">
+                            <div className="rating-inner"></div>
+                        </div>
+                        <span id="no_of_reviews">(5 Reviews)</span>
+                    </div>
+                        <p className="card-text">PHP 45.67</p>
+                        <a href="#" id="view_btn" className="btn btn-block">View Details</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+                <div className="card p-3 rounded">
+                    <img
+                    className="card-img-top mx-auto"
+                    src="../images/astro.png"
+                    />
+
+                    <div className="card-body d-flex flex-column">
+                        <h5 className="card-title">
+                            <a href="../images/astro.png">128GB Solid Storage Memory card - SanDisk Ultra</a>
+                        </h5>
+                    <div className="ratings mt-auto">
+                        <div className="rating-outer">
+                            <div className="rating-inner"></div>
+                        </div>
+                        <span id="no_of_reviews">(5 Reviews)</span>
+                    </div>
+                        <p className="card-text">PHP 45.67</p>
+                        <a href="#" id="view_btn" className="btn btn-block">View Details</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+                <div className="card p-3 rounded">
+                    <img
+                    className="card-img-top mx-auto"
+                    src="../images/astro.png"
+                    />
+
+                    <div className="card-body d-flex flex-column">
+                        <h5 className="card-title">
+                            <a href="../images/astro.png">AmazonBasics High-Speed HDMI Cable, 6 Feet</a>
+                        </h5>
+                    <div className="ratings mt-auto">
+                        <div className="rating-outer">
+                            <div className="rating-inner"></div>
+                        </div>
+                        <span id="no_of_reviews">(5 Reviews)</span>
+                    </div>
+                        <p className="card-text">PHP 45.67</p>
+                        <a href="#" id="view_btn" className="btn btn-block">View Details</a>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+        
+        </div>
+      
+      
+        </section>
+    </div>
+
+    )
+}
+
+export default Home
