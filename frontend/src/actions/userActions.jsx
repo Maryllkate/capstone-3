5c7ce1a0e2fd1cc9c46805e791d9de0a5b4325ad
maryll.driz <maryll.driz@gmail.com>
2023-09-22 14:53:53 +0800

    login.jsx fixed

--- frontend/src/actions/userActions.jsx
+++ frontend/src/actions/userActions.jsx
@@ -2,6 +2,9 @@ import {
     LOGIN_REQUEST,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
+    REGISTER_USER_REQUEST,
+    REGISTER_USER_SUCCESS,
+    REGISTER_USER_FAIL,
     CLEAR_ERRORS
 
 } from '../constants/userConstants'
@@ -17,11 +20,11 @@ export const login = (email, password) => async(dispatch) => {
 
         const config = {
             headers: {
-                'Content-type' : application/json
+                'Content-type' : 'application/json'
             }
         }
 
-        const { data } = await axios.post('/api/v1/login',{ email, password }, config )
+        const { data } = await axios.post('http://localhost:4000/api/v1/login',{ email, password }, config )
 
         dispatch({
             type: LOGIN_SUCCESS,
@@ -35,6 +38,34 @@ export const login = (email, password) => async(dispatch) => {
         })
     }
 }
+
+//new user
+export const register = (userData) => async(dispatch) => {
+    try {
+
+        dispatch({ type: REGISTER_USER_REQUEST })
+
+        const config = {
+            headers: {
+                'Content-type' : 'multipart/form-data'
+            }
+        }
+
+        const { data } = await axios.post('/api/v1/register',{ userData }, config )
+
+        dispatch({
+            type: REGISTER_USER_SUCCESS,
+            payload: data.user
+        })
+        
+    } catch (error) {
+        dispatch({
+            type: REGISTER_USER_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
 
     //errors
     export const clearErrors = () => async (dispatch) => {
